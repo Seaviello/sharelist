45f222a4992c11f3a541ece14ed2f4d77dd0ffa9
Tomasz Kawik <t.kawik@o2.pl>
2018-11-01 17:58:35 +0100

    Update packages. Build mainframe for application. Add babel support for styled components. Reset global some of the global stylings.

---
+++ frontend/components/Meta.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Meta = () => (
+        <Head>
+            <meta name='viewport' content='width-device-width, initial-scale=1'/>
+            <meta charSet='utf-8'/>
+            <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" />
+            <title>Sharelist</title>
+        </Head>
+    );
+
+Meta.displayName = 'Meta';
+
+export default Meta;
---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = props => (
+    <div>Initial page</div>
+);
+
+export default Home;
